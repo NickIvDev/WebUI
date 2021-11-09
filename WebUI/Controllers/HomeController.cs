423fcd63698c0a0f3d35202708aa214083625ad4
sparrow_n4k@mail.ru <sparrow_n4k@mail.ru>
2021-11-09 11:04:07 +0300

    Reliase

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using WebUI.Models;
 using WebUI.ViewModels;
 
@@ -79,95 +76,7 @@ namespace WebUI.Controllers
                 return View("GetCatalogsAhdFiles", model);
             }          
         }
-
-        [HttpPost]
-        public IActionResult GetSortDataBySize(string path, string sortBySize) // сортировка переданного каталога по размеру
-        {
-            GetCatalogsAhdFilesViewModel tempModel = GetDataViewModel(path);
-
-            if (sortBySize != null)
-            {
-                switch (sortBySize)
-                {
-                    case "ascending":
-                        List<CatalogModel> c = new List<CatalogModel>();
-                        foreach (var item in tempModel.Catalogs)
-                        {
-                            if (item.NoAccessToFile == false)
-                            {
-                                c.Add(item);
-                            }
-                        }
-                        List<FileModel> f = new List<FileModel>();
-                        foreach (var item in tempModel.Files)
-                        {
-                            if (item.NoAccessToFile == false)
-                            {
-                                f.Add(item);
-                            }
-                        }
-                        GetCatalogsAhdFilesViewModel m = new GetCatalogsAhdFilesViewModel
-                        {
-                            Path = path,
-                            Catalogs = c.OrderBy(c => c.SizeElementToKb).ToList(),
-                            Files = f.OrderBy(f => f.SizeElementToKb).ToList(),
-                        };
-                        return View("GetCatalogsAhdFiles", m);
-
-                    case "descending":
-                        List<CatalogModel> c1 = new List<CatalogModel>();
-                        foreach (var item in tempModel.Catalogs)
-                        {
-                            if (item.NoAccessToFile == false)
-                            {
-                                c1.Add(item);
-                            }
-                        }
-                        List<FileModel> f1 = new List<FileModel>();
-                        foreach (var item in tempModel.Files)
-                        {
-                            if (item.NoAccessToFile == false)
-                            {
-                                f1.Add(item);
-                            }
-                        }
-                        GetCatalogsAhdFilesViewModel m1 = new GetCatalogsAhdFilesViewModel
-                        {
-                            Path = path,
-                            Catalogs = c1.OrderByDescending(c => c.SizeElementToKb).ToList(),
-                            Files = f1.OrderByDescending(f => f.SizeElementToKb).ToList(),
-                        };
-                        return View("GetCatalogsAhdFiles", m1);
-
-                    case "noAccessToFile":
-                        List<CatalogModel> c2 = new List<CatalogModel>();
-                        foreach (var item in tempModel.Catalogs)
-                        {
-                            if (item.NoAccessToFile == true)
-                            {
-                                c2.Add(item);
-                            }
-                        }
-                        List<FileModel> f2 = new List<FileModel>();
-                        foreach (var item in tempModel.Files)
-                        {
-                            if (item.NoAccessToFile == true)
-                            {
-                                f2.Add(item);
-                            }
-                        }
-                        GetCatalogsAhdFilesViewModel m2 = new GetCatalogsAhdFilesViewModel
-                        {
-                            Path = path,
-                            Catalogs = c2.ToList(),
-                            Files = f2.ToList(),
-                        };
-                        return View("GetCatalogsAhdFiles", m2);
-                }
-            }
-            return default;
-        }
-
+        
         static GetCatalogsAhdFilesViewModel GetDataViewModel(string path) // получение модели представления по указанному пути
         {
             try
@@ -203,59 +112,14 @@ namespace WebUI.Controllers
                     catalogs.Add(new CatalogModel
                     {
                         Name = name,
-                        PathName = item,
+                        PathName = item, //= tempData,
                         Image = "/images/folder.jpg",
                         NoAccessToFile = false
                     });
+
+                    
                 }
-
-                // рекурсия - получение размера каталогов
-                foreach (var item in catalogs)
-                {
-                    double sizeCatalog = 0;
-                    sizeCatalog = SizeCatalog(item.PathName, ref sizeCatalog);
-
-                    // для более точного отслеживания нуля
-                    double testSizeCatalog = sizeCatalog;
-
-                    // отлавливаем каталоги без доступа(или с файлами без доступа)
-                    if (testSizeCatalog == 0)
-                    {
-                        try
-                        {
-                            List<string> c = Directory.GetDirectories(item.PathName).ToList();
-                            List<string> f = Directory.GetFiles(item.PathName).ToList();
-                            if (c.Count() != 0 || f.Count() != 0)
-                            {
-                                item.NoAccessToFile = true;
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            item.NoAccessToFile = true;
-                        }
-                    }
-
-                    if (testSizeCatalog >= 1024 * 1024 * 1024)
-                    {
-                        item.TypeOfSize = "GB";
-                        item.SizeElement = Math.Round((double)(sizeCatalog / 1024 / 1024 / 1024), 2);
-                        item.SizeElementToKb = Math.Round((double)(sizeCatalog / 1024), 2);
-                    }
-                    else if (testSizeCatalog >= 1024 * 1024)
-                    {
-                        item.TypeOfSize = "MB";
-                        item.SizeElement = Math.Round((double)(sizeCatalog / 1024 / 1024), 2);
-                        item.SizeElementToKb = Math.Round((double)(sizeCatalog / 1024), 2);
-                    }
-                    else
-                    {
-                        item.TypeOfSize = "KB";
-                        item.SizeElement = Math.Round((double)(sizeCatalog / 1024), 2);
-                        item.SizeElementToKb = Math.Round((double)(sizeCatalog / 1024), 2);
-                    }
-                }
-
+                
                 // файлы
                 foreach (var item in pcFiles)
                 {
@@ -343,76 +207,34 @@ namespace WebUI.Controllers
             }
         }
 
-        //public CatalogModel GetSizeCatalogForView(CatalogModel catalog) // for View
-        //{
-        //    double sizeCatalog = SizeCatalog2(catalog.PathName);
+        [HttpGet]
+        public double GetSizeCatalogForView(string path) // for View
+        {        
+            double sizeCatalog = 0;
+            sizeCatalog = SizeCatalog(path, ref sizeCatalog);
 
-        //    // для более точного отслеживания нуля
-        //    double testSizeCatalog = sizeCatalog;
+            // для более точного отслеживания нуля
+            double testSizeCatalog = sizeCatalog;
 
-        //    // отлавливаем каталоги без доступа(или с файлами без доступа)
-        //    if (testSizeCatalog == 0)
-        //    {
-        //        try
-        //        {
-        //            List<string> c = Directory.GetDirectories(catalog.PathName).ToList();
-        //            List<string> f = Directory.GetFiles(catalog.PathName).ToList();
-        //            if (c.Count() != 0 || f.Count() != 0)
-        //            {
-        //                catalog.NoAccessToFile = true;
-        //            }
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            catalog.NoAccessToFile = true;
-        //        }
-        //    }
-
-        //    if (testSizeCatalog >= 1024 * 1024 * 1024)
-        //    {
-        //        catalog.TypeOfSize = "GB";
-        //        catalog.SizeElement = Math.Round((double)(sizeCatalog / 1024 / 1024 / 1024), 2);
-        //        catalog.SizeElementToKb = Math.Round((double)(sizeCatalog / 1024), 2);
-        //    }
-        //    else if (testSizeCatalog >= 1024 * 1024)
-        //    {
-        //        catalog.TypeOfSize = "MB";
-        //        catalog.SizeElement = Math.Round((double)(sizeCatalog / 1024 / 1024), 2);
-        //        catalog.SizeElementToKb = Math.Round((double)(sizeCatalog / 1024), 2);
-        //    }
-        //    else
-        //    {
-        //        catalog.TypeOfSize = "KB";
-        //        catalog.SizeElement = Math.Round((double)(sizeCatalog / 1024), 2);
-        //        catalog.SizeElementToKb = Math.Round((double)(sizeCatalog / 1024), 2);
-        //    }
-        //    return catalog;
-        //}
-
-        //public static double SizeCatalog2(string path) // метод рекурсивного получения размера папок for View
-        //{
-        //    try
-        //    {
-        //        double sizeCatalog = 0;
-        //        DirectoryInfo di = new DirectoryInfo(path);
-        //        FileInfo[] fiA = di.GetFiles();
-        //        foreach (FileInfo fi in fiA)
-        //        {
-        //            sizeCatalog += fi.Length;
-        //        }
-
-        //        DirectoryInfo[] diA = di.GetDirectories();
-        //        foreach (DirectoryInfo d in diA)
-        //        {
-        //            sizeCatalog += SizeCatalog2(d.FullName);
-        //        }
-
-        //        return sizeCatalog;
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return 0;
-        //    }
-        //}
+            // отлавливаем каталоги без доступа(или с файлами без доступа)
+            if (testSizeCatalog == 0)
+            {
+                try
+                {
+                    List<string> c = Directory.GetDirectories(path).ToList();
+                    List<string> f = Directory.GetFiles(path).ToList();
+                    if (c.Count() != 0 || f.Count() != 0)
+                    {
+                        testSizeCatalog = -1;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    testSizeCatalog = -1;
+                }
+                return testSizeCatalog;
+            }         
+            return sizeCatalog;
+        }
     }
 }
--- WebUI/Models/CatalogModel.cs
+++ WebUI/Models/CatalogModel.cs
@@ -1,62 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using WebUI.Controllers;
+﻿//
 
 namespace WebUI.Models
 {
     public class CatalogModel : ForModel
-    {
-        public object GetSizeElement(CatalogModel catalog)
-        {            
-            // рекурсия - получение размера каталогов           
-            double sizeCatalog = 0;
-            //sizeCatalog = HomeController.SizeCatalog2(catalog.PathName);
-            sizeCatalog = HomeController.SizeCatalog(catalog.PathName, ref sizeCatalog);
-            
-            // для более точного отслеживания нуля
-            double testSizeCatalog = sizeCatalog;
-
-            // отлавливаем каталоги без доступа(или с файлами без доступа)
-            if (testSizeCatalog == 0)
-            {
-                try
-                {
-                    List<string> c = Directory.GetDirectories(catalog.PathName).ToList();
-                    List<string> f = Directory.GetFiles(catalog.PathName).ToList();
-                    if (c.Count() != 0 || f.Count() != 0)
-                    {
-                        catalog.NoAccessToFile = true;
-                    }
-                }
-                catch (Exception ex)
-                {
-                    catalog.NoAccessToFile = true;
-                }
-            }
-
-            if (testSizeCatalog >= 1024 * 1024 * 1024)
-            {
-                catalog.TypeOfSize = "GB";
-                catalog.SizeElement = Math.Round((double)(sizeCatalog / 1024 / 1024 / 1024), 2);
-                catalog.SizeElementToKb = Math.Round((double)(sizeCatalog / 1024), 2);
-            }
-            else if (testSizeCatalog >= 1024 * 1024)
-            {
-                catalog.TypeOfSize = "MB";
-                catalog.SizeElement = Math.Round((double)(sizeCatalog / 1024 / 1024), 2);
-                catalog.SizeElementToKb = Math.Round((double)(sizeCatalog / 1024), 2);
-            }
-            else
-            {
-                catalog.TypeOfSize = "KB";
-                catalog.SizeElement = Math.Round((double)(sizeCatalog / 1024), 2);
-                catalog.SizeElementToKb = Math.Round((double)(sizeCatalog / 1024), 2);
-            }
-            return default;
-        }        
+    {        
     }
 }
